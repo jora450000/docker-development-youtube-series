9f0e663647ccbb5b4ef75c10f223710f4a8d2662
Eugene Buravtsov <54440895+jora450000@users.noreply.github.com>
2025-07-25 11:09:33 +0300

    Update app.py

--- kubernetes/fluxcd/repositories/example-app-1/src/app.py
+++ kubernetes/fluxcd/repositories/example-app-1/src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import platform
+import socket
+version = "1.0.0.7"
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! v1.0.0.5"
+    return f"Hello World version:{version}, host: {platform.uname()[1]},  IP: {socket.gethostbyname(socket.gethostname())} !"
