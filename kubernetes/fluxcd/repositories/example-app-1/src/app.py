9c9a133d1d7e1d82213b33cfe47e09a1b091461e
root <root@compute-vm-4-8-30-ssd-1753163572318.ru-central1.internal>
2025-07-27 12:11:03 +0000

    modified:   ../src/app.py

--- kubernetes/fluxcd/repositories/example-app-1/src/app.py
+++ kubernetes/fluxcd/repositories/example-app-1/src/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 import platform
 import socket
-version = "1.0.0.7"
+version = "1.0.0.10"
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return f"Hello World version:{version}, host: {platform.uname()[1]},  IP: {socket.gethostbyname(socket.gethostname())} !"
+ return f"Hello World version:{version}, host: {platform.uname()[1]},  IP: {socket.gethostbyname(socket.gethostname())} !\n "
